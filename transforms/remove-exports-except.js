f70fa59e6ce9b19c7737aa1e2cc25da5e0a5e64f
Steve Faulkner <southpolesteve@gmail.com>
2017-12-29 14:45:54 -0500

    Add files

---
+++ transforms/remove-exports-except.js
@@ -0,0 +1,14 @@
+module.exports = function transformer (file, api, options) {
+  if (!options.except) { throw new Error('Must provide an export name to keep') }
+  const j = api.jscodeshift
+  const root = j(file.source)
+
+  root.find(j.ExportNamedDeclaration).forEach(path => {
+    const name = j(path).find(j.Identifier).nodes()[0].name
+    if (name !== options.except) {
+      j(path).remove()
+    }
+  })
+
+  return root.toSource()
+}
